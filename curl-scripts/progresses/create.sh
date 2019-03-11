d651b1274aa3e94005a7215532b6fa2cf697d5fb
allanolive <allan.loveforfood@gmail.com>
2019-03-11 13:37:11 -0400

    Added require token header to create.sh

--- curl-scripts/progresses/create.sh
+++ curl-scripts/progresses/create.sh
@@ -2,6 +2,7 @@ curl "http://localhost:4741/progresses" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "progress": {
       "weight": "'"${WEIGHT}"'",
