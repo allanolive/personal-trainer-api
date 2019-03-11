dc22af50bad511ce35f11d4fd81f3798a109718a
allanolive <allan.loveforfood@gmail.com>
2019-03-11 11:21:10 -0400

    Added edit and new route exceptions

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # RESTful routes
-  resources :examples, except: %i[new edit]
-
   # Custom routes
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out' => 'users#signout'
   patch '/change-password' => 'users#changepw'
+
+  # RESTful routes
+  resources :examples, except: %i[new edit]
+  resources :progresses, except: %i[new edit]
 end
